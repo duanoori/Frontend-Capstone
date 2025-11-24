e332e9767b20bc970877360fadeccf945f64d14b
duanoori <dua.noor1011@hotmail.com>
2025-11-24 21:59:43 +0500

    pass availableTimes and update handler to BookingForm via props

--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import BookingForm from "./BookingForm";
 
-function BookingPage() {
+function BookingPage({availableTimes, dispatch}) {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Reserve a Table</h1>
       <p>Fill out the form below to book your table at Little Lemon.</p>
 
-      <BookingForm />
+      <BookingForm 
+        availableTimes={availableTimes}
+        dispatch={dispatch}
+      />
 
       <p>We look forward to serving you!</p>
     </div>
