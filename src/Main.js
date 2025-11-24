a8302e945ee0f0f4afdcb511bac5c09f0a3738f2
duanoori <dua.noor1011@hotmail.com>
2025-11-24 21:58:08 +0500

    pass availableTimes and update handler to BookingForm via props

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,25 @@
 import React, {useReducer} from "react";
 import BookingPage from "./components/BookingPage";
 
+function initializeTimes() {
+    return ["17:00", "18:00", "19:00", "20:00", "21:00"];
+}
+
+function updateTimes(state, action) {
+    if (action.type === 'UPDATE_TIMES') {
+        return ["17:00", "18:00", "19:00", "20:00", "21:00"];
+    }
+    return state;
+}
+
 function Main() {
-  return (
-    <div>
-      <BookingPage />
-    </div>
-  );
+    const [availableTimes, dispatch] = useReducer(updateTimes, [], initializeTimes);
+    return (
+        <div>
+        <BookingPage 
+        availableTimes={availableTimes}
+        dispatch={dispatch}
+        />
+        </div>
+    );
 }
