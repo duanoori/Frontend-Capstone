7826e60169ea5a1a1fec52fe245593a740108424
duanoori <dua.noor1011@hotmail.com>
2025-11-24 22:15:11 +0500

    route / to Main component to centralize state management

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Routes>
 
-          <Route path="/" element={<BookingPage />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </Router>
     </>
