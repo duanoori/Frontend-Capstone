172d7c0e46166fe6658b0c778e067c44a5e8f9e7
duanoori <dua.noor1011@hotmail.com>
2025-11-25 20:58:07 +0500

    Created a booking confirm page

---
+++ src/components/ConfirmedBooking.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ConfirmedBooking() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Booking Confirmed!</h1>
+      <p>Thank you for your reservation. We look forward to serving you at Little Lemon!</p>
+    </div>
+  );
+}
+
+export default ConfirmedBooking;
